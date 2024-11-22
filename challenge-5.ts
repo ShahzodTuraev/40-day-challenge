e469230845a10a7c34aa9ddcc64cb2c3d32c30f2
shon <turayevshahzodbek@gmail.com>
2024-11-22 21:00:51 +0900

    challenge-5: 1072. Flip Columns For Maximum Number of Equal Rows (medium)

--- challenge-5.ts
+++ challenge-5.ts
@@ -1,15 +1,18 @@
-const countMap = new Map<string, number>();
-let maxEqualRows = 0;
+// 1072. Flip Columns For Maximum Number of Equal Rows
+function maxEqualRowsAfterFlips(matrix: number[][]): number {
+  const countMap = new Map<string, number>();
+  let maxEqualRows = 0;
 
-for (const row of matrix) {
-  if (row[0] === 1) {
-    for (let i = 0; i < row.length; i++) {
-      row[i] ^= 1;
+  for (const row of matrix) {
+    if (row[0] === 1) {
+      for (let i = 0; i < row.length; i++) {
+        row[i] ^= 1;
+      }
     }
+    const rowString = row.join("");
+    countMap.set(rowString, (countMap.get(rowString) || 0) + 1);
+    maxEqualRows = Math.max(maxEqualRows, countMap.get(rowString)!);
   }
-  const rowString = row.join("");
-  countMap.set(rowString, (countMap.get(rowString) || 0) + 1);
-  maxEqualRows = Math.max(maxEqualRows, countMap.get(rowString)!);
-}
 
-return maxEqualRows;
+  return maxEqualRows;
+}
