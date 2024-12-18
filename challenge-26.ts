50dfe4cb3eedd4d479b1fd285eaea00d601715d4
shon <turayevshahzodbek@gmail.com>
2024-12-19 01:25:41 +0900

    challenge-26: 1475. Final Prices With a Special Discount in a Shop (easy)

---
+++ challenge-26.ts
@@ -0,0 +1,19 @@
+// 1475. Final Prices With a Special Discount in a Shop
+function finalPrices(prices: number[]): number[] {
+  const lengthOfPrices = prices.length;
+  const discountedPrices = new Array(lengthOfPrices);
+  const priceStack: number[] = [];
+  for (let i = lengthOfPrices - 1; i >= 0; i--) {
+    const currentPrice = prices[i];
+    while (
+      priceStack.length !== 0 &&
+      priceStack[priceStack.length - 1] > currentPrice
+    ) {
+      priceStack.pop();
+    }
+    discountedPrices[i] =
+      currentPrice - (priceStack[priceStack.length - 1] ?? 0);
+    priceStack.push(currentPrice);
+  }
+  return discountedPrices;
+}
