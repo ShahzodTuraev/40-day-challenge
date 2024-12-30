5e1ee827398325626a996cb1a2b3dc95aa5e0bc9
shon <turayevshahzodbek@gmail.com>
2024-12-30 21:31:26 +0900

    challenge-33: 806. Number of Lines To Write String (easy)

---
+++ challenge-34.ts
@@ -0,0 +1,43 @@
+// 806. Number of Lines To Write String
+function numberOfLines(widths: number[], s: string): number[] {
+  const letters: string[] = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  let simpleLine: number = 0;
+  let lineLength: number = 1;
+  for (let i: number = 0; i < s.length; i++) {
+    const currentPixel = widths[letters.indexOf(s[i])];
+    if (simpleLine + currentPixel <= 100) {
+      simpleLine += currentPixel;
+    } else {
+      simpleLine = currentPixel;
+      lineLength += 1;
+    }
+  }
+  return [lineLength, simpleLine];
+}
